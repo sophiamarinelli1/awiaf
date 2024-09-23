6dae0d3f55da4b21ebdf7e7bd8d784f7cdfd2786
Sophia Marinelli <phiamarinelli@gmail.com>
2024-09-23 09:51:13 -0400

    footer fix

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<footer className=" sm:p-4 lg:p-12 flex flex-col gap-4 justify-between font-customMed leading-relaxed w-full text-black">
+		<footer className=" sm:p-4 lg:p-12 flex flex-col gap-4 justify-between font-customBold leading-relaxed w-full text-black">
 			<Link
 				href="https://sophiamarinelli.com"
 				className="flex flex-row justify-between">
