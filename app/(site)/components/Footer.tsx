8ca7f6ebcfb7a18ff6efbb79e796a9cf7f8bcf01
Sophia Marinelli <phiamarinelli@gmail.com>
2024-09-23 09:56:29 -0400

    anotha one

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
 		<footer className=" sm:p-4 lg:p-12 flex flex-col gap-4 justify-between font-customBold leading-relaxed w-full text-black">
 			<Link
 				href="https://sophiamarinelli.com"
-				className="flex flex-row justify-between">
-				<p className="">© 2024 ScM →</p>
+				className="flex sm:flex-col lg:flex-row sm:justify-start lg:justify-between">
+				<p className="sm:pb-4 lg:pb-0">© 2024 ScM →</p>
 				<p className="">
 					“Beyond all ideas of right and wrong there is a field, I will be
 					meeting you there.”
